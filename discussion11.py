1285e94ff634d16db11ea7ac7cfd7e943a687897
allyscoding <allyc@umich.edu>
2022-11-16 18:03:57 -0500

    commit

--- discussion11.py
+++ discussion11.py
@@ -39,12 +39,16 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
-    pass
+    cur.execute("drop table if exists Patients")
+    cur.execute("create table Patients(pet_id INTEGER PRIMARY KEY, name TEXT, species_id NUMBER, age INTEGER, cuteness INTEGER, aggressiveness INTEGER)")
+    conn.commit()
+
 
 
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
-    pass
+    cur.execute("insert into Patients(pet_id, name, species_id, age, cuteness, aggressiveness) values(?,?,?,?,?,?)", [0, "Fluffle", 0, 3, 90, 100])
+    conn.commit()
     
 
 # TASK 2
@@ -58,13 +62,25 @@ def add_pets_from_json(filename, cur, conn):
     f.close()
     json_data = json.loads(file_data)
 
+    pet_id=0
+    for item in json_data:
+        pet_id +=1
+        names = item["name"]
+        species = item["species"]
+        ages = item["age"]
+        cuteness = item["cuteness"]
+        aggressiveness = item["aggressiveness"]
+    
+    cur.execute("insert into Patients (pet_id, name, species_id, age, cuteness, aggressiveness) values(?,?,?,?,?,?)", [pet_id, names, species, ages, cuteness, aggressiveness])
+    conn.commit()
     # THE REST IS UP TO YOU
-    pass
+
 
 
 # TASK 3
 # CODE TO OUTPUT NON-AGGRESSIVE PETS
 def non_aggressive_pets(aggressiveness, cur, conn):
+
     pass
 
 
